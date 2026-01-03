89418c65d6ff3c11dc5ea59c6045eaea509cc66c
goutam-gold <lovetcs143@gmail.com>
2026-01-03 15:10:12 +0530

    cors change

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,14 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://creativeshowcaseforartist.onrender.com"
+  ],
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
